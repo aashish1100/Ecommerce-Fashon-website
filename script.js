a65d649864f061475a0010ef51f78bfefb8648d5
Aashish gupta <aashishgupta1100@gmail.com>
2024-07-27 19:43:33 +0530

    responsive home page

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 // Script for navigation bar
+const menue = document.querySelector("#bar");
+const navbar = document.querySelector("#navbar");
+const close = document.querySelector("#close");
+menue.addEventListener("click",function()
+{
+    navbar.classList.add('active');
+})
+
+close.addEventListener("click",function()
+{
+    navbar.classList.remove("active");
+})
