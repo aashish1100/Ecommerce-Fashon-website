fcd1079482c33b1c3937d5df743e823d058a63f6
Aashish gupta <aashishgupta1100@gmail.com>
2024-07-28 16:22:42 +0530

    created shop page

--- script.js
+++ script.js
@@ -11,3 +11,14 @@ close.addEventListener("click",function()
 {
     navbar.classList.remove("active");
 })
+
+var Mainimg = document.querySelector('#MainImg');
+var smallimgs = document.querySelectorAll(".small-img");
+
+for(let smallimg of smallimgs)
+{
+   smallimg.onclick =function()
+   {
+    Mainimg.src=smallimg.src;
+   }
+}
